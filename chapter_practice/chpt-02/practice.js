e1b0a2a045c9740909ea95e837b800d734e96158
Dennis Kimble <dennis.kimble@du.edu>
2017-06-24 11:29:53 -0500

    Added: Standard JavaScript for  ICT-4570

--- chapter_practice/chpt-02/practice.js
+++ chapter_practice/chpt-02/practice.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+/* 
+*  Name:		Dennis Kimble
+*  Filename:	practice.js
+*/
+
+// utility function to assign object id
+var $ = function(id) {
+	"use strict";
+	return document.getElementById(id);
+};
+
+// start you JS here
+
