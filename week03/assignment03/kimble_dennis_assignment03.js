0590971c423853ac0234032ff28a24b7a1e6ca32
Dennis Kimble <dennis.kimble@du.edu>
2017-07-03 07:07:44 -0500

    Updated: added all functions to application need to work on accessing associative array of  names and votes

--- week03/assignment03/kimble_dennis_assignment03.js
+++ week03/assignment03/kimble_dennis_assignment03.js
@@ -1,38 +1,25 @@
 /*	Name:  		Dennis Kimble
 *	Filename: kimble_dennis_assignment03.js
 */
-/*
-Be sure to add JavaScript logic to catch errors, or other, incorrect input and properly handle it:
-Unparsable dates and other errors should be reported or corrected upon pressing the calculate button.
-
-Your JavaScript functions should accomplish the following:
-
-
-
-
-A function to collect all the vote counts and return an Array, ordered by candidate, of their vote counts.
-A function to (a) collect all the counts; (b) get the sum of all votes; (c) compute the percentages for each candidate; and (d) place that result on the form in the correct locations.
-A function to initialize the form by setting the date to the current date, and attaching the click action to the click button.
-
-NOTES:
-Please ensure the user sees error text in red
-Be sure to handle the following situations explicitly (comment if no code is required):
-Input is blank
-Number input is not a number
-Number input is zero or negative
-Date is not parseable (see note below)
-Include screenshots of the cases you're handling through JavaScript code.
-If you post errors, warnings, or log messages to the console, include the console in the screenshot.
-Some browsers enable the input type to be selected to prevent illegal values from being selected. You are welcome to use these, but please remember that browsers are NOT obligated to support these, and some will just treat them as input text fields (so you still need to code the checks).
-Remember that getting elements by id, class, or tag name is pretty straightforward.
-
-
-input fields of type date need an ISO-formatted date. Unfortunately, JavaScript doesn't have a function to take care of this (yet). The following function will return a date as an ISO string, suitable to assigning to the input field's value property. 
-*/
 "use_strict";
+// variables for date functions
+var today = new Date();
+var isodate = toISODate(today);
 // A function to look up an HTML element by id and return the element
 var $ = function(id) {
 	return document.getElementById(id);
+};
+
+// A function to collect all the vote counts and return an Array
+// Ordered by candidate, of their vote counts.
+var getCandidateNamesAndVotes = function() {
+	// associative array with names as keys
+	var candidateNameArray = [];
+	candidateNameArray[$("candidate1").value] = $("votes1").value;
+	candidateNameArray[$("candidate2").value] = $("votes2").value;
+	candidateNameArray[$("candidate3").value] = $("votes3").value;
+	
+	return candidateNameArray;
 };
 
 // A function which takes an Array of numbers as a parameter, and returns the sum.
@@ -63,15 +50,123 @@ var toPercent = function(decimalNum) {
 // A function which takes an Array of numbers as a parameter, and returns an array of percentages
 // To compute a percentage from a vote count, simply divide the candidate's vote count by the total count
 var returnPercentArray = function(numArray) {
-	var percentArray = [0,0,0];
-	for (var x in numArray) {
-		percentArray[x] = numArray[x] / returnSum(numArray);
-	}
+	var percentArray = [];
 	
-	// To format a percentage, multiply the number by 100
+	// To format a percentage
 	for (var x in numArray) {
-		percentArray[x] = toPercent(percentArray[x]);
+		percentArray[x] = toPercent(numArray[x] / returnSum(numArray));
 	}
-
+	//alert(percentArray[1]);
 	return percentArray;
 };
+
+// A function to:
+//	(a) collect all the counts; 
+//	(b) get the sum of all votes; 
+//	(c) compute the percentages for each candidate;
+//	(d) place that result on the form in the correct locations.
+var processVotes = function() {
+	var isValid = true;
+	
+	if ( ($("candidate1").value === "") || ($("candidate2").value === "") || ( $("candidate3").value === "") ) {
+			$("candidate1_error").firstChild.nodeValue = "No boxes can be blank";
+			$("candidate2_error").firstChild.nodeValue = "No boxes can be blank";
+			$("candidate3_error").firstChild.nodeValue = "No boxes can be blank";
+			isValid = false;
+		// tested for blank values in candidate name boxes
+		} else {
+			$("candidate1_error").firstChild.nodeValue = "*";
+			$("candidate2_error").firstChild.nodeValue = "*";
+			$("candidate3_error").firstChild.nodeValue = "*";
+		}
+	if ( (isNaN($("votes1").value)) || (isNaN($("votes2").value)) || (isNaN($("votes3").value)) ) {
+			$("vote1_error").firstChild.nodeValue = "All votes must be numeric";
+			$("vote2_error").firstChild.nodeValue = "All votes must be numeric";
+			$("vote3_error").firstChild.nodeValue = "All votes must be numeric";
+			isValid = false;
+		// tested that vote is numeric
+		} else {
+			$("vote1_error").firstChild.nodeValue = "*";
+			$("vote2_error").firstChild.nodeValue = "*";
+			$("vote3_error").firstChild.nodeValue = "*";
+		}
+	if ( ($("votes1").value === "") || ($("votes2").value === "") || ( $("votes3").value === "") ) {
+			$("vote1_error").firstChild.nodeValue = "No boxes can be blank";
+			$("vote2_error").firstChild.nodeValue = "No boxes can be blank";
+			$("vote3_error").firstChild.nodeValue = "No boxes can be blank";
+			isValid = false;
+		// tested for blank values in voter boxes
+		} else {
+			$("vote1_error").firstChild.nodeValue = "*";
+			$("vote2_error").firstChild.nodeValue = "*";
+			$("vote3_error").firstChild.nodeValue = "*";
+		}
+	if ( ($("votes1").value < 0) || ( $("votes2").value < 0 ) || ( $("votes3").value < 0 ) ) {
+			$("vote1_error").firstChild.nodeValue = "Number must be positive or zero";
+			$("vote2_error").firstChild.nodeValue = "Number must be positive or zero";
+			$("vote3_error").firstChild.nodeValue = "Number must be positive or zero";
+			isValid = false;
+		// tested that number is positive
+		} else {
+			$("vote1_error").firstChild.nodeValue = "*";
+			$("vote2_error").firstChild.nodeValue = "*";
+			$("vote3_error").firstChild.nodeValue = "*";
+		}
+	if (!isValid) {
+		return;
+	}
+
+	// if all the checks pass, process the form
+	var rawVotes = [];
+		rawVotes[0] = parseInt($("votes1").value);
+		rawVotes[1] = parseInt($("votes2").value);
+		rawVotes[2] = parseInt($("votes3").value);
+	var votePercents = returnPercentArray(rawVotes);
+	var namesAndVotes = getCandidateNamesAndVotes();
+	
+	setDate();
+	
+	// set total text box
+	$("total_votes").firstChild.nodeValue = returnSum(rawVotes);
+	
+	// set candidate one name and vote percent
+	$("candidate_one").firstChild.nodeValue = $("candidate1").value;
+	$("percent1").firstChild.nodeValue =  votePercents[0];
+	
+	// set candidate one name and vote percent
+	$("candidate_two").firstChild.nodeValue = $("candidate2").value;
+	$("percent2").firstChild.nodeValue =  votePercents[1];
+	
+	// set candidate one name and vote percent
+	$("candidate_three").firstChild.nodeValue = $("candidate3").value;
+	$("percent3").firstChild.nodeValue =  votePercents[2];	
+};
+
+function toISODate(date) { // yyyy-mm-dd
+  "use strict";
+  var yyyy, mm, dd;
+  // JavaScript provides no simple way to format a date-only
+  yyyy = "" + date.getFullYear();
+  mm = date.getMonth() + 1; // Months go from 0 .. 11
+  dd = date.getDate();
+  // Need leading zeroes to form the yyyy-mm-dd pattern.
+  if (mm < 10) {
+    mm = "0" + mm; // This converts it to a string
+  }
+  if (dd < 10) {
+    dd = "0" + dd; // This converts it to a string
+  }
+  return "" + yyyy + "-" + mm + "-" + dd;
+};
+
+var setDate = function() {
+	$("date_at_click").value = isodate;
+};
+
+window.onload = function() {
+	$("calculate").onclick = processVotes;
+	$("candidate1").focus();
+	
+};
+
+
