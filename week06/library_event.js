c3baa365dbbcbe356d48e640a5c3ac1132566515
Dennis Kimble <dennis.kimble@du.edu>
2017-07-19 14:01:00 -0500

    Added: Cross-Browser compatability event handler

---
+++ week06/library_event.js
@@ -0,0 +1,25 @@
+// Javascript file
+var evt = {
+	attach: function(node, eventName, func) {
+		"use strict";
+		if (node.addEventListener) {
+			node.addEventListener(eventName, func);
+		} else if (node.attachEvent) {
+			node.attachEvent("on" + eventName, func);
+		}
+	},
+	detach: function(node, eventName, func) {
+		"use strict";
+		if (node.removeEventListener) {
+			node.removeEventListener(eventName, func);
+		} else if (node.detachEvent) {
+			node.detachEvent("on" + eventName, func);
+		}
+	},
+	preventDefault: function(e) {
+		"use strict";
+		e = e || window.event;
+		if (e.preventDefault) { e.preventDefault(); }
+		else { e.returnValue = false; }
+	}
+};
