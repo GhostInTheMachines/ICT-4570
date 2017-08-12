8b17e5e8ed1e11dc9d53073eb4dd6da420e96278
Dennis Kimble <dennis.kimble@du.edu>
2017-08-12 08:14:33 -0500

    Revised: String handler for Headers to allow for multi-word attribute conversions

--- week08/assignment06/kimble_dennis_assignment06.js
+++ week08/assignment06/kimble_dennis_assignment06.js
@@ -1,11 +1,23 @@
-// Table building script Adapted from mountain example at url: http://schwartzcomputer.com/ICT4570/Resources/mountainTable.html
-//  XMLHttpRequest script adapted from Presidents example at url: http://schwartzcomputer.com/ICT4570/Resources/president.html
-// Updated by: Dennis Kimble
+// Table building script Adapted from mountain example at 
+// url: http://schwartzcomputer.com/ICT4570/Resources/mountainTable.html
+// XMLHttpRequest script adapted from Presidents example at 
+// url: http://schwartzcomputer.com/ICT4570/Resources/president.html
+
+// Name: Dennis Kimble
 // File: kimble_dennis_assignment06.js
 
+// Capitalize first character of single word
+// or both words if separated by underscores
 var capitalize = function(s) {
  "use strict";
- return s.charAt(0).toUpperCase() + s.slice(1);
+ if (s.indexOf('_') === -1) {
+	 return s.charAt(0).toUpperCase() + s.slice(1);
+ }else {  // if attribute has a dash between words remove it
+	 var lastHalf = s.substr(s.indexOf('_')+1);
+	 var firstHalf = s.substr(0, s.indexOf('_'));
+	 return firstHalf.charAt(0).toUpperCase() + firstHalf.slice(1) + " " +
+	 		lastHalf.charAt(0).toUpperCase() + lastHalf.slice(1);
+ }
 };	  
 var buildHeader = function(titles,trAtt,thAtt) {
  "use strict";
@@ -14,13 +26,13 @@ var buildHeader = function(titles,trAtt,thAtt) {
     tr.setAttribute('class',trAtt);
   }
   len = titles.length;
-  for ( i=0; i<len; i+=1) {
+  	for ( i=0; i<len; i+=1) {
  	  th = document.createElement('th');
  	  if (thAtt) {
       th.setAttribute('class',thAtt);
     }
- 	    th.appendChild(document.createTextNode(capitalize(titles[i])));
- 	  tr.appendChild(th);
+ 	th.appendChild(document.createTextNode(capitalize(titles[i])));
+ 	tr.appendChild(th);
   }
   return tr;
 };
